171cfbf9ca0f5093eb2cc47c2dd09528a525be92
Mario Granada <mario.granada@globant.com>
2020-02-20 22:59:37 -0500

    add user form

--- users-to-do-ui/src/shared/components/UserList/UserList.js
+++ users-to-do-ui/src/shared/components/UserList/UserList.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
-
+import { FormControl, Button, TextField } from '@material-ui/core';
 import './UserList.scss';
 import UserItem from './UserItem/UserItem';
 import {
   listUsers,
   updateUser,
-  removeUser
+  removeUser,
+  createUser
 } from '../../services/usersToDoAPI/usersToDoAPI';
 
 const UserList = () => {
   const [usersState, setUsersState] = useState([]);
-  const [expandedState, setExpandedState] = React.useState(false);
+  const [expandedState, setExpandedState] = useState(false);
+
+  const [newUserState, setNewUserState] = useState({});
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
 
   const handleChange = panel => (event, isExpanded) => {
     setExpandedState(isExpanded ? panel : false);
@@ -40,9 +44,69 @@ const UserList = () => {
     });
   };
 
+  const onCreateUserHandler = () => {
+    createUser(newUserState).then(({ data }) => {
+      console.log('on create', data);
+      setUsersState([...usersState, data]);
+    });
+  };
+
   return (
     <div className="user-list">
       <h1 className="list-title">Users</h1>
+      <div className="new-user-box">
+        <FormControl>
+          <Button
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              setIsCreatingUser(true);
+              setNewUserState({});
+            }}
+          >
+            Add User
+          </Button>
+        </FormControl>
+
+        {isCreatingUser && (
+          <div className="new-user-form">
+            <FormControl>
+              <TextField
+                onChange={e => {
+                  setNewUserState({
+                    ...newUserState,
+                    name: e.target.value
+                  });
+                }}
+                value={newUserState.name || ''}
+                label="Username"
+              />
+            </FormControl>
+            <FormControl>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                onClick={onCreateUserHandler}
+              >
+                Create User
+              </Button>
+            </FormControl>
+            <FormControl>
+              <Button
+                size="small"
+                onClick={() => {
+                  setIsCreatingUser(false);
+                }}
+                variant="outlined"
+              >
+                Cancel
+              </Button>
+            </FormControl>
+          </div>
+        )}
+      </div>
       <div className="users-list-box">
         {usersState.map(item => (
           <UserItem
